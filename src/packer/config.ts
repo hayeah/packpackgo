52d568d602dc7d2288da254b4fdea820deb1972c
Howard Yeh <howard@metacircus.com>
2016-09-19 17:29:56 +0800

    hack to get webpack to load modules from packpackgo

--- src/packer/config.ts
+++ src/packer/config.ts
@@ -44,6 +44,28 @@ export async function configureWebpack(project: Project, useProductionBundle = f
 		"index": "index.js",
 	}, options as any);
 
+	// Webpack a lookup by recursively look up node_modules, but this works only for the project's context.
+	//
+	// The root and fallback settings ask Webpack to look for modules in these directories, but without recursive node_modules lookup.
+	//
+	// The order webpack resolves a module is:
+	//
+	// 1. Lookup in root directories.
+	// 2. Recursively lookup node_modules.
+	// 3. Lookup in fallback directories.
+	//
+	// It's better to include batteries in the fallback directories, so user can install node_modules.
+	// config.resolveLoader.root
+	config.resolveLoader.fallback = [
+		path.join(__packagedir, "node_modules"),
+		path.join(__packagedir, "node_modules", "quickpack", "node_modules"),
+	];
+
+	// "battery included" modules, like react, react-dom, normalize.css
+	config.resolve.fallback = [
+		path.join(__packagedir, "node_modules"),
+	];
+
 
 
 	const progressPlugin = new ProgressPlugin(project.updateProgress.bind(project));
